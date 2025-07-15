e1bfa0396c2aa7b10695774c23e3f951ee79fe7f
daffadon <daffaputranarendra9@gmail.com>
2025-07-15 14:58:41 +0700

    refactor(config): debug error

--- config/storage/minio.go
+++ config/storage/minio.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"fmt"
 	"log"
+	"net"
 
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
@@ -10,8 +11,17 @@ import (
 )
 
 func NewMinioConnection() *minio.Client {
+
 	hostPort := viper.GetString("minio.host") + ":" + viper.GetString("minio.port")
-	fmt.Print(hostPort)
+	fmt.Print("Checking MinIO connectivity: ", hostPort, "\n")
+
+	// Connectivity check
+	conn, err := net.Dial("tcp", hostPort)
+	if err != nil {
+		log.Fatalf("Cannot connect to MinIO at %s: %v", hostPort, err)
+	}
+	conn.Close()
+
 	minioClient, err := minio.New(hostPort, &minio.Options{
 		Creds: credentials.NewStaticV4(
 			viper.GetString("minio.credential.user"),
